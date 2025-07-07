3cf55a48bfbea98d320220493266da0a1f67db1c
manyugoyal <148210818+manyugoyal@users.noreply.github.com>
2025-07-07 07:49:49 +0530

    work

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,18 +1,21 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyCQbx_SoLd_2g35C6Oq8s0N2qXA0BLriZY",
   authDomain: "travel-1d156.firebaseapp.com",
   projectId: "travel-1d156",
-  storageBucket: "travel-1d156.firebasestorage.app",
+  storageBucket: "travel-1d156.appspot.com", 
   messagingSenderId: "413197910426",
   appId: "1:413197910426:web:8298356cf27a7beeac3a23",
-  measurementId: "G-PXY38VW1KE"
+  measurementId: "G-PXY38VW1KE",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app); 
+export const provider = new GoogleAuthProvider(); 
